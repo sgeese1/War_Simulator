f92f74e014d28a438f9f647920ed2d025517acf4
sgeese1 <geesey1@live.com>
2021-11-11 19:32:45 -0500

    Updated war simulator and added blackjack simulator to repo

--- war.py
+++ war.py
@@ -1,10 +1,18 @@
 import PyDealer as pd
 
+class Player:
+    hand = pd.Stack
+    
+
 def play_War():
     deck = pd.Deck()
     deck = pd.Shuffle()
     player1 = pd.Stack()
     player2 = pd.Stack()
+
+
+
+def war_time():
 
 
 def start_Game(num_Iters):
